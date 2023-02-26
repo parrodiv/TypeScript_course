373c55ba5f62a41b3af817b205e78dac19561789
Alessandro Parrilla <paggio96@gmail.com>
2023-02-26 21:08:16 +0100

    07- optional properties

--- lesson_7_IndexSignatures_keyofAssertion_RecordUtilityType/src/main.ts
+++ lesson_7_IndexSignatures_keyofAssertion_RecordUtilityType/src/main.ts
@@ -1,14 +1,10 @@
 // Index Signature
 // Two main reasons why Index Signature are useful:
 
-// 1 - Are useful when you're creating an object but you don't know the exact names of the object keys but you do know the shape of the object
+// 1 - They are useful when you're creating an object but you don't know the exact names of the object keys but you do know the shape of the object
 // You can declare the type of the keys and the type of the values
 
 // 2- Typescript requires an index signature if you attempt to access an object property dinamically
-
-interface TransactionObj {
-  readonly [index: string]: number // INDEX SIGNATURE
-}
 
 // interface TransactionObj {
 //   Pizza: number
@@ -16,10 +12,24 @@ interface TransactionObj {
 //   Job: number
 // }
 
+// interface TransactionObj {
+//   readonly [index: string]: number // INDEX SIGNATURE
+// }
+
+interface TransactionObj {
+  readonly [index: string]: number
+  Pizza: number,
+  Books: number,
+  Job: number
+}
+
+
+
 const todayTransactions: TransactionObj = {
   Pizza: -10,
   Books: -5,
   Job: 50,
+  Ale: 27 // Ale is not required but the other 3 are, because they are defined in interface
 }
 
 console.log(todayTransactions.Pizza) // -10
@@ -34,9 +44,8 @@ console.log(todayTransactions[prop])
 const todaysNet = (transactions: TransactionObj): number => {
   let total = 0
   for (const transaction in transactions) {
-    total += transactions[transaction] // * <--- here
-    // Err: Element implicitly has an 'any' type because expression of type 'string' can't be used to index type 'TransactionObj'.
-    // No index signature with a parameter of type 'string' was found on type 'TransactionObj'
+    total += transactions[transaction] 
+   
   }
   return total
 }
@@ -46,9 +55,24 @@ console.log(todaysNet(todayTransactions))
 // if there is "readonly" before [index] it can't be reassigned
 // console.log(todayTransactions.Pizza = 20)
 
-// Non existing properties returns undefined
-// because it has no way to know what names we would give the keys
-// because rememeber our interface just says it's going to be an index of string and it should return a number
-// so TypeScript really thinks this is going to return a number but it's going to be undefined
-// this does open up the possibility for you to try to access a key on an object that does not exist
-console.log(todayTransactions['Ale'])  // UNDEFINED
+// now it works 
+console.log(todayTransactions['Ale'])  // 27
+
+// ************************************************
+
+interface Student {
+  [key: string]: string | number | number[] | undefined
+  // I have to insert all this types because they are all present in this interface
+  // also undefined because classes is optional
+  name: string,
+  GPA: number,
+  classes?: number[] // optional
+}
+
+const student: Student = {
+  name: "Karl",
+  GPA: 3.5,
+  classes: [100, 200]
+}
+
+console.log(student.test)
