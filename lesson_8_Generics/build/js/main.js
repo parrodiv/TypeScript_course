9bff3d4e24b512494147da4c2e802729aa617413
Alessandro Parrilla <alessandroparrilla@MacBook-Pro-di-Alessandro.local>
2023-02-28 14:14:29 +0100

    08- Generics: basic example and syntax, isObject example, isTrue example

---
+++ lesson_8_Generics/build/js/main.js
@@ -0,0 +1,33 @@
+"use strict";
+// Generics
+const stringEcho = (arg) => arg;
+const echo = (arg) => arg;
+// Generics allow creating 'type variables' which can be used to create classes, functions & type aliases that don't need to explicitly define the types that they use. Generics makes it easier to write reusable code.
+const isObj = (arg) => {
+    return typeof arg === 'object' && !Array.isArray(arg) && arg !== null;
+};
+// console.log(isObj(true))
+// console.log(isObj('John'))
+// console.log(isObj({ name: 'John' }))
+// console.log(isObj(null))
+const isTrue = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { arg, is: false };
+    }
+    if (isObj(arg) && !Object.keys(arg).length) {
+        // console.log(!Object.keys(arg as keyof T).length)
+        return { arg, is: false };
+    }
+    return { arg, is: !!arg };
+    // '!!' transform one value in boolean (for example if I pass 0 it will be false, an empty string it will be false and so on)
+};
+console.log(isTrue({ ok: 'ok' }));
+console.log(isTrue(false));
+console.log(isTrue(0));
+console.log(isTrue(true));
+console.log(isTrue(1));
+console.log(isTrue('Ale'));
+console.log(isTrue('false'));
+console.log(isTrue(null));
+console.log(isTrue(undefined));
+console.log(isTrue({}));
