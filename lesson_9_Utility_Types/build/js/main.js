f9352080ab763c0653e60ec7812078dd5bf6d54c
Alessandro Parrilla <alessandroparrilla@MacBook-Pro-di-Alessandro.local>
2023-03-01 10:23:57 +0100

    NonNullable and ReturnType

--- lesson_9_Utility_Types/build/js/main.js
+++ lesson_9_Utility_Types/build/js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 // Utility types
-// * Partial allow us to to specify some of the props in the Assignment interface, not every
+//  Partial allow us to to specify some of the props in the Assignment interface, not every
 const updateAssignment = (assign, propsToUpdate) => {
     return Object.assign(Object.assign({}, assign), propsToUpdate);
 };
@@ -11,18 +11,18 @@ const assign1 = {
 };
 console.log(updateAssignment(assign1, { grade: 95 }));
 const assignGraded = updateAssignment(assign1, { grade: 95 });
-// REQUIRED and READONLY
-// * Required specify that it requires all of the property, also those that are set as optional
+// * REQUIRED and READONLY
+//  Required specify that it requires all of the property, also those that are set as optional
 const recordAssignment = (assign) => {
     // send to database, etc.
     return assign;
 };
 const assignVerified = Object.assign(Object.assign({}, assignGraded), { verified: true });
-// * Readonly can not be modified
+// Readonly can not be modified
 // assignVerified.grade = 88
 //recordAssingment requires all props
 console.log(recordAssignment(Object.assign(Object.assign({}, assignGraded), { verified: true })));
-// RECORD
+// * RECORD
 const hexColorMap = {
     // * Record<keyType, valueType>
     red: 'FF0000',
@@ -44,3 +44,14 @@ const score = {
 const scorePreview = {
     title: 'score'
 };
+// it exclude null and undefined
+// * ReturnType
+// type NewAssign = {title: string, points: number}
+// const createNewAssign = (title: string, points: number): NewAssign => {
+//   return {title, points}
+// }
+const createNewAssign = (title, points) => {
+    return { title, points };
+};
+const tsAssign = createNewAssign("Utility Types", 100);
+console.log(tsAssign);
--- lesson_9_Utility_Types/src/main.ts
+++ lesson_9_Utility_Types/src/main.ts
@@ -1,6 +1,6 @@
 // Utility types
 
-// PARTIAL
+// * PARTIAL
 
 interface Assignment {
   studentId: string
@@ -9,7 +9,7 @@ interface Assignment {
   verified?: boolean
 }
 
-// * Partial allow us to to specify some of the props in the Assignment interface, not every
+//  Partial allow us to to specify some of the props in the Assignment interface, not every
 
 const updateAssignment = (
   assign: Assignment,
@@ -27,9 +27,9 @@ const assign1: Assignment = {
 console.log(updateAssignment(assign1, { grade: 95 }))
 const assignGraded: Assignment = updateAssignment(assign1, { grade: 95 })
 
-// REQUIRED and READONLY
+// * REQUIRED and READONLY
 
-// * Required specify that it requires all of the property, also those that are set as optional
+//  Required specify that it requires all of the property, also those that are set as optional
 const recordAssignment = (assign: Required<Assignment>): Assignment => {
   // send to database, etc.
   return assign
@@ -37,13 +37,13 @@ const recordAssignment = (assign: Required<Assignment>): Assignment => {
 
 const assignVerified: Readonly<Assignment> = { ...assignGraded, verified: true }
 
-// * Readonly can not be modified
+// Readonly can not be modified
 // assignVerified.grade = 88
 
 //recordAssingment requires all props
 console.log(recordAssignment({ ...assignGraded, verified: true }))
 
-// RECORD
+// * RECORD
 const hexColorMap: Record<string, string> = {
   // * Record<keyType, valueType>
   red: 'FF0000',
@@ -73,7 +73,7 @@ const gradeData: Record<Students, Grades> = {
 
 
 
-// PICK and OMIT
+// * PICK and OMIT
 
 // Pick dà la possibilità di scegliere che parametri utilizzare di un oggetto
 
@@ -92,7 +92,7 @@ const scorePreview: AssignPreview = {
 }
 
 
-// EXCLUDE and EXTRACT
+// * EXCLUDE and EXTRACT
 // They son't work with interface, but only with string literal union types
 // over the mouse on adjustedGrade and highGrades to see results
 
@@ -101,4 +101,29 @@ type adjustedGrade = Exclude<LetterGrades, "U">
 type highGrades = Extract<LetterGrades, "A" | "B">
 
 
+// * NonNullable
+
+type AllPossibleGrades = 'Dave' | 'John' | null | undefined
+
+type NamesOnly = NonNullable<AllPossibleGrades>
+// it exclude null and undefined
+
+
+// * ReturnType
+
+// type NewAssign = {title: string, points: number}
+
+// const createNewAssign = (title: string, points: number): NewAssign => {
+//   return {title, points}
+// }
+
+const createNewAssign = (title:string, points: number) => {
+  return {title, points}
+}
+
+// viene impostato il tipo NewAssign con il tipo di dato ritornato dalla funzione createNewAssign 
+type NewAssign = ReturnType<typeof createNewAssign>
+
+const tsAssign: NewAssign = createNewAssign("Utility Types", 100)
+console.log(tsAssign)
 
