605c40f7eeedfc6eb17d473ed377197d9f2ca6b8
Alessandro Parrilla <alessandroparrilla@MacBook-Pro-di-Alessandro.local>
2023-03-06 22:43:20 +0100

    Recommended Code and default value

--- lesson_11_ReactTypescript/src/App.tsx
+++ lesson_11_ReactTypescript/src/App.tsx
@@ -1,10 +1,15 @@
-import Heading from "./components/Heading"
+import Heading from './components/Heading'
+import { Section } from './components/Section'
 
 function App() {
- 
   return (
-    <Heading title={"Hello"}/>
-    )
+    <>
+      <Heading title={'Hello'} />
+      <Section>
+        This is my Section
+      </Section>
+    </>
+  )
 }
 
 export default App
--- lesson_11_ReactTypescript/src/components/Section.tsx
+++ lesson_11_ReactTypescript/src/components/Section.tsx
@@ -1,11 +1,25 @@
-import React from "react"
+import React, { ReactNode } from "react"
 
 // OLD CODE 
-const Section: React.FC<{title: string}> = ({ children, title}) => {
-  return (
-    <section>
-      <h2>{title}</h2>
-      <p>{children}</p>
-    </section>
-  )
+// const Section: React.FC<{title: string}> = ({ children, title}) => {
+//   return (
+//     <section>
+//       <h2>{title}</h2>
+//       <p>{children}</p>
+//     </section>
+//   )
+// }
+
+type SectionProps = {
+  title?: string,
+  children: ReactNode
+}
+
+export const Section = ({title = 'My Subheading', children}: SectionProps) => {
+ return (
+  <section>
+    <h2>{title}</h2>
+    <p>{children}</p>
+  </section>
+ )
 }
