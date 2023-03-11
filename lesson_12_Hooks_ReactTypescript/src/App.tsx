debf71845b24f39466da89439ce1f72798334af0
Alessandro Parrilla <paggio96@gmail.com>
2023-03-11 18:09:17 +0100

    useEffect and fucntion with event parameter

--- lesson_12_Hooks_ReactTypescript/src/App.tsx
+++ lesson_12_Hooks_ReactTypescript/src/App.tsx
@@ -1,4 +1,10 @@
-import React, { useState } from "react"
+import React, {
+  useState,
+  useEffect,
+  useCallback,
+  MouseEvent,
+  KeyboardEvent,
+} from 'react'
 
 interface User {
   id: string
@@ -9,14 +15,33 @@ const App = () => {
   const [count, setCount] = useState<number>(0)
   const [users, setUsers] = useState<User | null>(null)
 
+  useEffect(() => {
+    console.log('mounting')
+
+    console.log({ users })
+
+    return () => console.log('unmounting')
+  })
+
+  // memoize a function so every time  the component rerender, the function isn't going to be recreated
+  const addTwo = useCallback(() => {
+    setCount((prev) => prev + 2)
+  }, [])
+
+  // let's assume that in the function it is needed an event, with Typescript it should look as follow:
+  const funcEvent = useCallback(
+    (e: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>) => {
+      console.log(e.target)
+    },
+    []
+  )
+
   return (
-   <>
-   ciao
-   </>
+    <>
+      <h1>{count}</h1>
+      <button onClick={addTwo}>Add</button>
+    </>
   )
 }
 
 export default App
-
-
-
