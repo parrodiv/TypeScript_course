afd98a5a05252a15d2d24cf22c13fa777df8ec5e
Alessandro Parrilla <alessandroparrilla@MacBook-Pro-di-Alessandro.local>
2023-03-13 16:53:16 +0100

    Refactoring from useState to useReducer

--- lesson_13_useReducerTypescript/src/Counter.tsx
+++ lesson_13_useReducerTypescript/src/Counter.tsx
@@ -1,21 +1,44 @@
 import { ReactNode, useState, useReducer } from 'react'
 
+const initState = { count: 0 }
 
+const enum REDUCER_ACTION_TYPE {
+  INCREMENT,
+  DECREMENT,
+}
 
+type ReducerAction = {
+  type: REDUCER_ACTION_TYPE
+}
+
+const reducer = (
+  state: typeof initState,
+  action: ReducerAction
+): typeof initState => {
+  switch (action.type) {
+    case REDUCER_ACTION_TYPE.INCREMENT:
+      return { ...state, count: state.count + 1 }
+    case REDUCER_ACTION_TYPE.DECREMENT:
+      return { ...state, count: state.count - 1 }
+    default:
+      throw new Error()
+  }
+}
 
 type ChildrenType = {
   children: (num: number) => ReactNode
 }
 
 const Counter = ({ children }: ChildrenType) => {
-  const [count, setCount] = useState<number>(1)
+  // const [count, setCount] = useState<number>(1)
+  const [state, dispatch] = useReducer(reducer, initState)
 
-  const increment = () => setCount((prev) => prev + 1)
-  const decrement = () => setCount((prev) => prev - 1)
+  const increment = () => dispatch({ type: REDUCER_ACTION_TYPE.INCREMENT })
+  const decrement = () => dispatch({ type: REDUCER_ACTION_TYPE.DECREMENT })
 
   return (
     <>
-      <h1>{children(count)}</h1>
+      <h1>{children(state.count)}</h1>
       <div>
         <button onClick={increment}>+</button>
         <button onClick={decrement}>-</button>
