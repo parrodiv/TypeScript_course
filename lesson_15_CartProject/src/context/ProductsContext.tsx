e612ef54bf0f8d01a1490b2d20af24f577c6149b
Alessandro Parrilla <alessandroparrilla@MacBook-Pro-di-Alessandro.local>
2023-03-21 08:58:42 +0100

    Promise, Fetch and json-server with npx json-server -w data/products.json -p 3500

--- lesson_15_CartProject/src/context/ProductsContext.tsx
+++ lesson_15_CartProject/src/context/ProductsContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, ReactElement } from 'react'
+import { createContext, useState, ReactElement, useEffect } from 'react'
 
 type ProductType = {
   sku: string
@@ -6,24 +6,25 @@ type ProductType = {
   price: number
 }
 
-export const initState: ProductType[] = [
-  {
-    sku: 'item0001',
-    name: 'Widget',
-    price: 9.99,
-  },
-  {
-    sku: 'item0002',
-    name: 'Widget',
-    price: 14.99,
-  },
-  {
-    sku: 'item0003',
-    name: 'Widget',
-    price: 18.99,
-  },
+const initState: ProductType[] = []
+// export const initState: ProductType[] = [
+//   {
+//     sku: 'item0001',
+//     name: 'Widget',
+//     price: 9.99,
+//   },
+//   {
+//     sku: 'item0002',
+//     name: 'Widget',
+//     price: 14.99,
+//   },
+//   {
+//     sku: 'item0003',
+//     name: 'Widget',
+//     price: 18.99,
+//   },
    
-]
+// ]
 
 type UseProductsContextType = {
   products: ProductType[]
@@ -42,6 +43,18 @@ type ChildrenType = {
 
 export const CounterProvider = ({children}: ChildrenType): ReactElement => {
   const [products, setProducts] = useState<ProductType[]>(initState)
+
+  useEffect(() => {
+    const fetchProducts = async (): Promise<ProductType[]> => {
+      const data = await fetch('http://localhost:3500/products').then(res => {
+        return res.json()
+      }).catch(err => {
+        if (err instanceof Error) console.log(err.message)
+      })
+      return data
+    }
+    fetchProducts().then(products => setProducts(products))
+  }, [])
 
   return (
     <ProductContext.Provider value={{products}}>
